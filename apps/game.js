8eb804dc401c0a0b58b5274a44da266ce37e059a
Saren Currie <saren@sarencurrie.com>
2016-02-18 10:04:30 +1300

    Added comments, fixed missing semicolons, fixed most indentation, improved code style, improved log messages.

--- apps/game.js
+++ apps/game.js
@@ -6,7 +6,9 @@ var uuid = require('node-uuid');
 var createGame = function (io, room) {
 	console.log('creating namespace ' + room.roomName);
 
+	// Create socket.io namespace for room
 	var game = io.of(room.roomName);
+
 	var facesRecieved = false;
 	var relicRequirement = [10,15,20,30,40];
 	var sacrificeRequirement = [2,3,4,5,5];
@@ -22,6 +24,7 @@ var createGame = function (io, room) {
 
 		console.log(sId + ' joined ' + room.roomName);
 
+		// Initialise player proprties
 		room.players[sId].ready = false;
 		room.players[sId].cards = {};
 		room.players[sId].board = {};
@@ -33,13 +36,14 @@ var createGame = function (io, room) {
 
 		var cardSet = cards({
 			summonCard: function (summoner, summonee) {
-				console.log('summoning...');
+				console.log('summoning card');
 
 				var pId = summoner.player;
 				var cardId = uuid.v4();
 				var card = {};
 
 				if (_.size(room.players[pId].board) > 6) {
+					// Board full
 					return;
 				}
 
@@ -51,15 +55,16 @@ var createGame = function (io, room) {
 			},
 			killCard: function (card) {
 				console.log('Killing card: ' + card.name);
-				room.players[card.player].board[card.id].causeOfDeath = 'damage'
-				var dedCard = room.players[card.player].board[card.id];
-				room.graveyard.push(dedCard);
+				room.players[card.player].board[card.id].causeOfDeath = 'damage';
+				var deadCard = room.players[card.player].board[card.id];
+				room.graveyard.push(deadCard);
 				room.players[card.player].board[card.id].deathCry(room);
 				// Remember to edit this for both sides.
 				for (var key in room.players[card.player].board) {
 					room.players[card.player].board[key].onEvent(room, 'death');
 				}
 			},
+			// @FIXME: Rename this method
 			itsReallyOver: function (card) {
 				delete room.players[card.player].board[card.id];
 				game.emit('cardKilled', card);
@@ -80,7 +85,7 @@ var createGame = function (io, room) {
 			},
 			earnDamage: function (card, amount, self){
 				var toAward = card.player;
-				if (self){
+				if (self) {
 						if (card.player == player1){
 							toAward = player2;
 						} else {
@@ -92,49 +97,46 @@ var createGame = function (io, room) {
 			}
 		});
 
-		// console.log('Printing the deck!')
-		// console.log(_.size(cardSet));
-		// console.log(cardSet)
 		//this is kind of a patchy fix for players also being minions. TODO!
-		for (var i = 0; i < 30; i++){
-			while (true){
+		for (var i = 0; i < 30; i++) {
+			while (true) {
 				var num = Math.floor(Math.random() * (_.size(cardSet)));
 				var count = 0;
 				var cardFound = false;
 				for (var key in cardSet) {
-					if (count == num){
-					if (cardSet[key].type == 'player'){
-						break
+					if (count == num) {
+						if (cardSet[key].type == 'player'){
+							break;
+						}
+						if (cardSet[key].token){
+							break;
+						}
+						if (cardSet[key].name == 'Sleeping Statue'){
+							break;
+						}
+						var cardId = uuid.v4();
+						var card = {};
+						_.extend(card, cardSet[key], {player: sId, id: cardId});
+						room.players[sId].deck[i]  = card;
+						cardFound = true;
 					}
-					if (cardSet[key].token){
-						break
-					}
-					if (cardSet[key].name == 'Sleeping Statue'){
-						break
-					}
-					var cardId = uuid.v4();
-					var card = {};
-					_.extend(card, cardSet[key], {player: sId, id: cardId});
-					room.players[sId].deck[i]  = card;
-					cardFound = true;
-				}
-				count += 1;
+					count += 1;
 				}
 				if (cardFound == true){
 					if (i < 3){
-						room.players[sId].mullcards[i] = room.players[sId].deck[i].id
+						room.players[sId].mullcards[i] = room.players[sId].deck[i].id;
 					}
-					break
+					break;
 				}
 			}
 		}
-		console.log('printing deck!')
-		console.log(room.players[sId].deck)
-		console.log(_.size(room.players[sId].deck))
+		console.log('printing deck!');
+		console.log(room.players[sId].deck);
+		console.log(_.size(room.players[sId].deck));
 
 		if (!player1) {
 			player1 = sId;
-			var cardId = uuid.v4();
+			var cardId = uuid.v4(); // Generate unique id
 			var card = {};
 			_.extend(card, cardSet.player1, {player: sId, id: cardId});
 			room.players[sId].board[cardId] = card;
@@ -142,7 +144,7 @@ var createGame = function (io, room) {
 		}
 		else {
 			player2 = sId;
-			var cardId = uuid.v4();
+			var cardId = uuid.v4(); // Generate unique id
 			var card = {};
 			_.extend(card, cardSet.player2, {player: sId, id: cardId});
 			room.players[sId].board[cardId] = card;
@@ -156,9 +158,9 @@ var createGame = function (io, room) {
 		}
 
 		socket.on('endTurn', function () {
+			console.log('End of turn');
 			var sId = id(socket.id);
 			var nextPlayer = sId === player1 ? player2 : player1;
-			console.log('EoT');
 			for (var key in room.players[sId].board) {
 				room.players[sId].board[key].endOfTurn(room);
 			}
@@ -188,7 +190,7 @@ var createGame = function (io, room) {
 				room.players[nextPlayer].board[key].startOfTurn(room);
 			}
 
-			refresh(nextPlayer)
+			refresh(nextPlayer);
 
 			game.emit('newTurn', {
 				turnPlayer: nextPlayer,
@@ -197,6 +199,7 @@ var createGame = function (io, room) {
 			});
 		});
 
+		// @TODO: This function should probably be moved somewhere else.
 		function pickRandomProperty(obj) {
 			var result;
 			var count = 0;
@@ -208,26 +211,26 @@ var createGame = function (io, room) {
 			return obj[result];
 		}
 
-		function hitFace(playerToAward,howMuch){
+		function hitFace(playerToAward,howMuch) {
 			room.players[playerToAward].facedamage += howMuch;
-			while (room.players[playerToAward].facedamage > relicRequirement[room.players[playerToAward].relics]){
-				room.players[playerToAward].facedamage -= relicRequirement[room.players[playerToAward].relics]
-				room.players[playerToAward].relics += 1
-				console.log('relic earned!')
+			while (room.players[playerToAward].facedamage > relicRequirement[room.players[playerToAward].relics]) {
+				room.players[playerToAward].facedamage -= relicRequirement[room.players[playerToAward].relics];
+				room.players[playerToAward].relics += 1;
+				console.log('relic earned!');
 				game.emit('relicEarned', playerToAward, room.players[playerToAward].relics);
-				if (room.players[playerToAward].relics >= 5){
-					game.emit('gameOver', playerToAward)
+				if (room.players[playerToAward].relics >= 5) {
+					game.emit('gameOver', playerToAward);
 				}
 			}
 			game.emit('faceDamageEarned', playerToAward, room.players[playerToAward].facedamage);
 		}
 
 		function drawCard(data, list) {
-			var tempsID
-			if (data){
-				tempsID = data.player
+			var tempsID;
+			if (data) {
+				tempsID = data.player;
 			} else {
-				tempsID = sId
+				tempsID = sId;
 			}
 			console.log('player ' + tempsID + ' draws');
 			if (_.size(	room.players[tempsID].cards) < 10) {
@@ -240,14 +243,14 @@ var createGame = function (io, room) {
 						socket.emit('cardDrawn', card);
 					}
 				} else {
-				var card = room.players[tempsID].deck.shift();
-				console.log('deck size is');
-				console.log(_.size(room.players[tempsID].deck));
-				room.players[tempsID].cards[card.id] = card;
+					var card = room.players[tempsID].deck.shift();
+					console.log('deck size is');
+					console.log(_.size(room.players[tempsID].deck));
+					room.players[tempsID].cards[card.id] = card;
 
-				socket.emit('cardDrawn', card);
-				game.emit('updateSize', tempsID, _.size(room.players[tempsID].deck));
-			}
+					socket.emit('cardDrawn', card);
+					game.emit('updateSize', tempsID, _.size(room.players[tempsID].deck));
+				}
 			}
 			else {
 				console.log('card "burnt"');
@@ -268,26 +271,27 @@ var createGame = function (io, room) {
 
 		socket.on('mulliganCard', function (data) {
 			if (_.contains(room.players[sId].mullcards,data.id)){
-			console.log(sId + ' mulliganed ' + data.name);
-			console.log(room.players[sId].deck);
-			while (true) {
-				var num = Math.floor(Math.random() * (_.size(room.players[sId].deck)));
-				console.log('random num: ' + num);
-				if (room.players[sId].deck[num].type != 'player'){
-					break;
+				console.log(sId + ' mulliganed ' + data.name);
+				console.log(room.players[sId].deck);
+				var num;
+				while (true) {
+					num = Math.floor(Math.random() * (_.size(room.players[sId].deck)));
+					console.log('random num: ' + num);
+					if (room.players[sId].deck[num].type != 'player'){
+						break;
+					}
 				}
+				var temp = room.players[sId].cards[data.id];
+				var card = room.players[sId].deck[num];
+				room.players[sId].cards[card.id] = card;
+				room.players[sId].deck[num] = temp;
+				delete room.players[sId].cards[data.id];
+				socket.emit('cardMulliganed', data);
+				socket.emit('cardDrawn', card);
+				game.emit('updateSize', sId, _.size(room.players[sId].deck));
+			} else {
+				console.log('Can\'t mulligan a card you just recieved');
 			}
-			var temp = room.players[sId].cards[data.id];
-			var card = room.players[sId].deck[num];
-			room.players[sId].cards[card.id] = card;
-			room.players[sId].deck[num] = temp;
-			delete room.players[sId].cards[data.id];
-			socket.emit('cardMulliganed', data);
-			socket.emit('cardDrawn', card);
-			game.emit('updateSize', sId, _.size(room.players[sId].deck));
-		} else {
-			console.log('Can\'t mulligan a card you just recieved');
-		}
 		});
 
 		socket.on('playCard', function (data, target) {
@@ -330,7 +334,7 @@ var createGame = function (io, room) {
 		});
 
 		socket.on('getFaces',  function() {
-			if (!facesRecieved){
+			if (!facesRecieved) {
 				facesRecieved = true;
 				for (var key in room.players) {
 					game.emit('cardPlayed', room.players[key].faces, 1);
@@ -339,11 +343,12 @@ var createGame = function (io, room) {
 		});
 
 		socket.on('sacrifice',  function(data) {
-			for (var i in sacrificedMinions){
-				if (data.id == sacrificedMinions[i].id){
-				console.log('Minion already sacrificed!');
-				return;
-			}
+			for (var i in sacrificedMinions) {
+				if (data.id == sacrificedMinions[i].id) {
+					// Minion has already been sacrificed
+					console.log('Minion already sacrificed!');
+					return;
+				}
 			}
 			var player = data.player;
 			console.log('Sacrificing minion!');
@@ -352,21 +357,21 @@ var createGame = function (io, room) {
 			console.log('Deathrow is:');
 			console.log(sacrificedMinions);
 			if (_.size(sacrificedMinions) > 1){
-				room.players[player].relics += 1
-				console.log('relic earned!')
+				room.players[player].relics += 1;
+				console.log('relic earned!');
 				game.emit('relicEarned', player, room.players[player].relics);
 				if (room.players[player].relics >= 5){
-					game.emit('gameOver', player)
+					game.emit('gameOver', player);
 				}
-				console.log('I AM DELETING:');
+				console.log('player sacrificed');
 				console.log(sacrificedMinions);
-				for (var i in sacrificedMinions){
+				for (var i in sacrificedMinions) {
 					minion = sacrificedMinions[i];
 					console.log('Killing card: ' + minion.name);
 					console.log(minion);
-					room.players[card.player].board[minion.id].causeOfDeath = 'sacrifice'
-					var dedCard = room.players[card.player].board[minion.id];
-					room.graveyard.push(dedCard);
+					room.players[card.player].board[minion.id].causeOfDeath = 'sacrifice';
+					var deadCard = room.players[card.player].board[minion.id];
+					room.graveyard.push(deadCard);
 					room.players[card.player].board[minion.id].deathCry(room);
 				}
 				sacrificedMinions = [];
@@ -375,12 +380,12 @@ var createGame = function (io, room) {
 		});
 
 		function activateBR(card, target) {
-			console.log("activating BR");
+			console.log('activating battlerattle');
 			console.log(card);
 			console.log(target);
 			console.log('check card on board');
 			console.log(room.players[card.player].board[card.id]);
-			console.log('check target on board')
+			console.log('check target on board');
 			console.log(room.players[target.player].board[target.id]);
 			room.players[card.player].board[card.id].battleRattle(room, room.players[target.player].board[target.id]);
 		}
@@ -392,21 +397,21 @@ var createGame = function (io, room) {
 			console.log(victim);
 			if (room.players[sId].board[attacker.id]) {
 				if (room.players[sId].board[attacker.id].attacks == 0) {
-					console.log("This minion can't attack!")
+					console.log('This minion can\'t attack!');
 					return;
 				}
 				if (room.players[sId].board[victim.id]) {
-					console.log("Can't attack your own minions!");
+					console.log('Can\'t attack your own minions!');
 					return;
 				}
-				if (room.players[sId].board[attacker.id].type == 'player'){
-					console.log(room.players[sId].board[attacker.id].type)
-					console.log("Can't attack with your own face! How did you even gain attack?");
+				if (room.players[sId].board[attacker.id].type == 'player') {
+					console.log(room.players[sId].board[attacker.id].type);
+					console.log('Can\'t attack with your own face! How did you even gain attack?');
 					return;
 				}
-				if (room.players[victim.player].board[victim.id].type == 'player'){
-					if (_.size(room.players[victim.player].board) > 1){
-						console.log("You must clear the board before going face!");
+				if (room.players[victim.player].board[victim.id].type == 'player') {
+					if (_.size(room.players[victim.player].board) > 1) {
+						console.log('You must clear the board before going face!');
 						return;
 					} else {
 						hitFace(sId,room.players[sId].board[attacker.id].damage);
@@ -418,17 +423,18 @@ var createGame = function (io, room) {
 				room.players[sId].board[attacker.id].attacks -= 1;
 				room.players[sId].board[attacker.id].attack(room, room.players[victim.player].board[victim.id]);
 			} else {
-				console.log("This minion doesn't belong to you!");
+				console.log('This minion doesn\'t belong to you!');
 			}
 		});
 	});
 
 	console.log('started');
 
+	// Tell player to join new namespace now that it has been created
 	io.emit('roomStarted', {
 		roomName: room.roomName,
 		players: room.players
 	});
-}
+};
 
 module.exports = createGame;
